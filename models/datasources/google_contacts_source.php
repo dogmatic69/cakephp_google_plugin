945410daf20ff2942a13a9e380f6d7bbcf0710bd
imekinox <jc.ekinox@gmail.com>
2009-12-14 18:13:23 -0600

    Divided basic api calls that are the same for all diferent google services into a google_source to avoid repeating code, gogole_contacts_source now extends to google_source to inherit authentication and server calls.

--- models/datasources/google_contacts_source.php
+++ models/datasources/google_contacts_source.php
@@ -12,19 +12,18 @@
 * @copyright     Copyright (c) 2009 Juan Carlos del Valle ( imekinox )
 * @link http://www.imekinox.com
 * @package       google
-* @subpackage    google.models.datasources.google_contacts
+* @subpackage    google.models.datasources.google.google_contacts
 * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
 */
 
-App::import('Core', 'HttpSocket');
-App::import('Core','Session');
+App::import('Datasource','GoogleSource');
 
 /**
 * GoogleContactsSource
 *
 * Datasource for Google Contacts
 */
-class GoogleContactsSource extends DataSource {
+class GoogleContactsSource extends GoogleSource {
 
   /**
   * Version for this Data Source.
@@ -43,33 +42,6 @@ class GoogleContactsSource extends DataSource {
   var $description = 'GoogleContacts Datasource';
 
   /**
-  * Client Login URL
-  *
-  * @var string
-  * @access private
-  */
-
-  private $_uri = "https://www.google.com/accounts/ClientLogin"; //you'll have to uncomment extension=php_openssl.dll from php.ini
-
-  /**
-  * Auth key returned by google API
-  *
-  * @var string
-  * @access private
-  */
-
-  private $_auth_key;
-
-  /**
-  * Method used to make requests (curl or file_get_contents)
-  *
-  * @var string
-  * @access private
-  */
-
-  private $_method;
-
-  /**
   * Default Constructor
   *
   * @param array $config options
@@ -77,82 +49,12 @@ class GoogleContactsSource extends DataSource {
   */
 
   public function __construct($config) {
-    $_toPost['accountType'] = $config['accountType'];
-    $_toPost['Email'] = $config['Email'];
-    $_toPost['Passwd'] = $config['Passwd'];
-    $_toPost['service'] = $config['service'];
-    $_toPost['source'] = $config['source'];
-
-    // Initializing Cake Session
-    $session = new CakeSession();
-    $session->start();
-    
-    // Validating if curl is available
-    if (function_exists('curl_init')) {
-      $this->_method = 'curl';
-    } else {
-      $this->_method = 'fopen';
-    }
-    
-    //Looking for auth key in cookie of google api client login
-    $cookie_key = $session->read('GoogleClientLogin._auth_key');
-    if($cookie_key == NULL || $cookie_key == ""){
-      $HttpSocket = new HttpSocket();
-      $results = $HttpSocket->post($this->_uri, $_toPost);
-      $first_split = split("\n",$results);
-      foreach($first_split as $string) {
-        $arr = split("=",$string);
-        if ($arr[0] == "Auth") $this->_auth_key = $arr[1];
-      }
-      $session->write('GoogleClientLogin._auth_key', $this->_auth_key);
-    } else {
-      $this->_auth_key = $cookie_key;
-    }
     parent::__construct($config);
-  }
-
-  /**
-  * Send Request
-  *
-  * @param string $url URL to do the request
-  * @param string $method GET or POST
-  * @return xml object
-  * @access private
-  */
-  private function sendRequest($url, $method) {
-    /*
-      Could'nt find a way to do it via $HttpSocket i got empty result
-
-      $auth['header'] = "Authorization: GoogleLogin auth=" . $this->_auth_key;
-      $result = $HttpSocket->get("http://www.google.com/m8/feeds/contacts/jc.ekinox@gmail.com/full", array(), $auth);
-    */
-    $header[] = "Authorization: GoogleLogin auth=" . $this->_auth_key;
-    $header[] = "GData-Version: 3.0";
-    if ($this->_method == 'curl') {
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-      $json = curl_exec($ch);
-      curl_close($ch);
-    } else {
-      $opts = array(
-                'http'=>array(
-                         'method'=>$method,
-                         'header'=>$header[0]
-                       )
-              );
-      $context = stream_context_create($opts);
-      $json = file_get_contents($url, false, $context);
-    }
-    $result = json_decode($json,true);
-    return $result;
   }
 
   public function read($model, $queryData = array()) {
     if (isset($queryData['conditions']['id'])) {
-      return $this->sendRequest("http://www.google.com/m8/feeds/contacts/default/full".$queryData['conditions']['id'], "GET");
+      return $this->sendRequest("http://www.google.com/m8/feeds/contacts/default/full/".$queryData['conditions']['id'], "GET");
     } else {
       $args['alt'] = 'json';
       $args['max-results'] = ($queryData['limit'] != null)?$queryData['limit']:'25';
@@ -181,18 +83,7 @@ class GoogleContactsSource extends DataSource {
 
   }
 
-  public function calculate(&$model, $func, $params = array()) {
-    $params = (array)$params;
-    switch (strtolower($func)) {
-    case 'count':
-      return array('COUNT' => true);
-      break;
-    case 'max':
-      break;
-    case 'min':
-      break;
-    }
-  }
+  // public function calculate(&$model, $func, $params = array()) { }
 
   public function query($query, $params, $model) {
     switch ($query) {
