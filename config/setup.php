fb7d015064029b7f45d1f49b4c8bad0214eaf4e3
dogmatic <dogmatic69@gmail.com>
2009-12-23 15:58:19 +0200

    fixing up some comments and copywrights

--- config/setup.php
+++ config/setup.php
@@ -1,4 +1,21 @@
 <?php
+    /**
+     * Google config class file.
+     *
+     * setup the google helper.
+     *
+     * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+     *
+     * Licensed under The MIT License
+     * Redistributions of files must retain the above copyright notice.
+     *
+     * @filesource
+     * @copyright     Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+     * @link          http://www.dogmatic.co.za
+     * @package       google
+     * @subpackage    google.setup
+     * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+     */
     class GoogleConfig
     {
         /**
--- google_app_controller.php
+++ google_app_controller.php
@@ -8,8 +8,21 @@
     );
 
     /**
+     * Google App Controller class file.
      *
+     * the google_appcontroller file, extends AppController
      *
+     * Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+     *
+     * Licensed under The MIT License
+     * Redistributions of files must retain the above copyright notice.
+     *
+     * @filesource
+     * @copyright     Copyright (c) 2009 Carl Sutton ( dogmatic69 )
+     * @link          http://www.dogmatic.co.za
+     * @package       google
+     * @subpackage    google.controllers.google_app_controller
+     * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
      */
     class GoogleAppController extends AppController
     {
